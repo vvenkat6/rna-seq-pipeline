226ee4941c16bb84454b0da09ac5b15f4e072d52
Vaishnavi Venkat <vv39@wl-10-190-233-143.wireless.duke.edu>
2016-03-24 14:32:25 -0400

    Trimmomatic changes to default parameters, Changes to cufflink options

--- RnaseqPipeline.py
+++ RnaseqPipeline.py
@@ -43,47 +43,49 @@ from subprocess import call
 #Global Variable:
 post = "False"
 
-def Quality_Assessment(read,kmer,logger):
+def Quality_Assessment(read,kmer,logger,out):
 	#Function to run Fastqc
 	logger.info('Running FastQC')
-	call(['fastqc','--kmers',str(kmer),'--extract',read,'--outdir','FastqcMetrics/'])	
+	call(['fastqc','--kmers',str(kmer),'--extract',read,'--outdir',out+"/FastqcMetrics/"])	
 
 def TrimmingPE(read1,read2,thread,phred,lead,trail,crop,minlen,window,qual,out,logger):
 	#Function to trim reads using Trimmomatic
 	logger.info('Trimming the reads')
-	call(["trimmomatic","PE","-trimlog","Trimmomatic.log","-threads",str(thread),"-"+phred,read1,read2,out+"_paired1.fq",out+"_paired2.fq",out+"_unpaired1.fq",out+"_unpaired2.fq","HEADCROP:"+str(crop),"LEADING:"+str(lead),"TRAILING:"+str(trail),"MINLEN:"+str(minlen),"SLIDINGWINDOW:"+str(window)+":"+str(qual)])
-	
+	#call(["trimmomatic","PE","-trimlog","Trimmomatic.log","-threads",str(thread),"-"+phred,read1,read2,out+"/"+out+"_paired1.fq",out+"/"+out+"_paired2.fq",out+"/"+out+"_unpaired1.fq",out+"/"+out+"_unpaired2.fq","HEADCROP:"+str(crop),"LEADING:"+str(lead),"TRAILING:"+str(trail),"MINLEN:"+str(minlen),"SLIDINGWINDOW:"+str(window)+":"+str(qual)])
+	call(["trimmomatic","PE","-trimlog","Trimmomatic.log","-threads",str(thread),"-"+phred,read1,read2,out+"/"+out+"_paired1.fq",out+"/"+out+"_paired2.fq",out+"/"+out+"_unpaired1.fq",out+"/"+out+"_unpaired2.fq","HEADCROP:"+str(crop),"LEADING:"+str(lead),"TRAILING:"+str(trail)])	
+
 def khmer(read1,read2,kmer,logger):
 	#function to normalize reads based on given kmer length
 	logger.info("Normalizing the given reads...")
 	call(["normalize-by-median.py","-k",kmer,read1,read2])
 
-def Kallisto(read1,read2,file,kmer,boost,thread,logger):
+def Kallisto(read1,read2,file,kmer,boost,thread,logger,out):
 	#Function to run Kallisto
 	logger.info('Starting kallisto Indexing step...')
 	call(["kallisto","index","-i","kallisto_indexfile.idx","-k",str(kmer),file])
 	
 	logger.info("Kallisto Indexing over. Starting Quantification...")
-	call(["kallisto","quant","-t",str(thread),"-b",str(boost),"-i","kallisto_indexfile.idx","-o","Kallisto_Output",read1,read2])
+	call(["kallisto","quant","-t",str(thread),"-b",str(boost),"-i","kallisto_indexfile.idx","-o",out+"/Kallisto_Output",read1,read2])
 
-def Bwa(read1,read2,file,algo,extra,logger):
+def Bwa(read1,read2,file,algo,extra,logger,out):
 	global post
 	#Function to run bwa
 	logger.info("Starting bwa indexing step...")
 	call(["bwa","index",file])
 
 	logger.info("Bwa Indexing over. Starting Quantification...")
-	cmd = "bwa "+algo+" "+file+" "+read1+" "+read2+" > Bwa_output.sam"
+	cmd = "bwa "+algo+" "+file+" "+read1+" "+read2+" > "+out+"/Bwa_output.sam"
 	os.system(cmd)
 	logger.info("Sorting sam file...")
-	cmd = "sort -k 1 Bwa_output.sam > Bwa.hits.sam.sorted"
+	cmd = "sort -k 1 "+out+"/Bwa_output.sam > "+out+"/Bwa.hits.sam.sorted"
 	os.system(cmd)
-	cmd = "echo 'Post Mapping Metrics' > PostMappingMetrics.txt"
+
+	cmd = "echo 'Post Mapping Metrics' > "+out+"/PostMappingMetrics.txt"
 	os.system(cmd)
-	PostMapping("Bwa.hits.sam.sorted",logger)
+	PostMapping(out+"/Bwa.hits.sam.sorted",logger,out)
 	if post=="True":
-		PostQuality("Bwa.hits.sam.sorted","Test_post",logger)
-	eXpress(file, "Bwa.hits.sam.sorted",extra,logger)
+		PostQuality(out+"/Bwa.hits.sam.sorted",out,logger)
+	eXpress(file, out+"/Bwa.hits.sam.sorted",extra,logger)
 
 def eXpress(file,sam,extra,logger):
 	#Function to run express analysis
@@ -101,41 +103,51 @@ def BowtieIndex(file,out,logger,libtype,bowalgo,read1,read2,thread,gtf,multi):
 	os.system(cmd)
 	TopHat(read1,read2,file,out,libtype,bowalgo,thread,logger,gtf,multi)
 
-def TopHat(read1,read2,bowref,out,libtype,bowalgo,thread,logger,gtf,multi):
+def TopHat(read1,read2,bowref,out,libtype,bowalgo,thread,logger,gtf,multi,mask):
 	#Function to run tophat
 	logger.info("Running TopHat...")
-	cmd = "tophat "+libtype+" "+bowalgo+" -o "+out+"_TopHat "+"-p "+str(thread)+" "+bowref+" "+read1+" "+read2
+	cmd = "tophat "+libtype+" "+bowalgo+" -o "+out+"/TopHat "+"-p "+str(thread)+" "+bowref+" "+read1+" "+read2
 	os.system(cmd)
-	cmd = "cat "+ out+"_TopHat/align_summary.txt > PostMappingMetrics.txt"
+	cmd = "cat "+ out+"/TopHat/align_summary.txt > "+out+"/PostMappingMetrics.txt"
 	os.system(cmd)
-	cmd = "echo ' ' >> PostMappingMetrics.txt"
+	cmd = "echo ' ' >> "+out+"/PostMappingMetrics.txt"
         os.system(cmd)
-        cmd = "echo ' ' >> PostMappingMetrics.txt"
+        cmd = "echo ' ' >> "+out+"/PostMappingMetrics.txt"
         os.system(cmd)
-	PostMapping(out+"_TopHat/accepted_hits.bam",logger)
+	PostMapping(out+"/TopHat/accepted_hits.bam",logger,out)
 	if post =="True":
-		PostQuality(out+"_TopHat/accepted_hits.bam",out+"_PostMappingGrap_",logger)
-	Cufflinks(out+"_TopHat/accepted_hits.bam",gtf,thread,libtype,multi,out,logger)
+		PostQuality(out+"/TopHat/accepted_hits.bam",out,logger)
+	Cufflinks(out+"/TopHat/accepted_hits.bam",gtf,thread,libtype,multi,out,logger)
 
-def Cufflinks(file,gtf,thread,libtype,multi,out,logger):
+def Cufflinks(file,gtf,thread,libtype,multi,out,logger,mask):
 	logger.info("Running Cufflinks...")
 	if gtf == "Na":
 		logger.warning("Running cufflinks without GTF file! This is not recommended")
 		gtf = ''
-	call(["cufflinks",file,"-o",out+"_Cufflinks","-u",multi,"--library-type",libtype,"-p",str(thread),"-G",gtf])
-	logger.info("Outputs can be accessed from "+out+"_Cufflinks/isoforms.fpkm_tracking")
+	else:
+		gtf = "--GTF-guide "+gtf
 
-def PostMapping(file,logger):
+	if mask == "Na":
+                logger.warning("Running cufflinks without mask file! This is NOT recommended")
+                mask = ''
+        else:
+                mask = " --mask-file  "+mask
+
+	call(["cufflinks",file,"-o",out+"/Cufflinks","-u",multi,"--library-type",libtype,"-p",str(thread),gtf,mask])
+	logger.info("Outputs can be accessed from "+out+"/Cufflinks/isoforms.fpkm_tracking")
+
+def PostMapping(file,logger,out):
 	logger.info("Doing Post Mapping analysis...")
-	cmd = "echo ' ' >> PostMappingMetrics.txt"
+	cmd = "echo ' ' >> "+out+"/PostMappingMetrics.txt"
 	os.system(cmd)
-	cmd = "echo ' ' >> PostMappingMetrics.txt"
+	cmd = "echo ' ' >> "+out+"/PostMappingMetrics.txt"
         os.system(cmd)
-	cmd = "samtools flagstat "+file+" >> PostMappingMetrics.txt"
+	cmd = "samtools flagstat "+file+" >> "+out+"/PostMappingMetrics.txt"
 	os.system(cmd)
-	logger.info("Post Mapping Metrics can be accessed from PostMappingMetrics.txt")
+	logger.info("Post Mapping Metrics can be accessed from "+out+"/PostMappingMetrics.txt")
 
 def PostQuality(file,out,logger):
+	out = out+"/"+out+"_"
 	#Function to reimplement Read_quality.py from RSEQC package written by Liguo Wang
 	logger.info("Plotting post quality graphs...")
 	if (os.path.exists(file)):
@@ -164,12 +176,12 @@ def main():
 	trimmomatic = parser.add_argument_group("Trimmomatic options")
 	parser.add_argument('--threads',dest="thread",help="Set the number of threads to use for trimmomatic [Default=4]",default=4,type=int,metavar="number(int)")
 	trimmomatic.add_argument('--phred33|phred64',dest="phred",default="phred64",choices=["phred33","phred64"])
-	trimmomatic.add_argument('--leading',dest="trimlead",help="Specify the minimum quality to keep a base towards the start of the sequence [Default = 25]", type=int, default=25,metavar="quality(int)")
-	trimmomatic.add_argument('--trailing',dest="trimtrail",help="Specify the minimum quality to keep a base towards the end of the sequence [Default = 25]",type=int,default=25,metavar="quality(int)")
+	trimmomatic.add_argument('--leading',dest="trimlead",help="Specify the minimum quality to keep a base towards the start of the sequence [Default = 20]", type=int, default=20,metavar="quality(int)")
+	trimmomatic.add_argument('--trailing',dest="trimtrail",help="Specify the minimum quality to keep a base towards the end of the sequence [Default = 20]",type=int,default=20,metavar="quality(int)")
 	trimmomatic.add_argument('--headcrop',dest="trimcrop",help="Specify the number of bases to remove, from the start of the read [Default = 0]",type=int,default=0,metavar="length(int)")	
 	trimmomatic.add_argument('--minlen',dest="trimlen",help="Specify the minimum lengths of read to keep [Default = 0]",type=int,default=0,metavar="length(int)")
-	trimmomatic.add_argument('--window',dest="trimwindow",help="Specify the number of bases to average across for sliding window [Default=10]",type=int,default=4,metavar="size(int)")
-	trimmomatic.add_argument('--quality',dest="trimq",help="Specify the average quality required [Default = 15]", type=int, default=15, metavar="quality(int)")
+	trimmomatic.add_argument('--window',dest="trimwindow",help="Specify the number of bases to average across for sliding window [Default=0]",type=int,default=0,metavar="size(int)")
+	trimmomatic.add_argument('--quality',dest="trimq",help="Specify the average quality required [Default = 0]", type=int, default=0, metavar="quality(int)")
 
 	kallisto = parser.add_argument_group("Fast Alginment Option - Kallisto")
 	kallisto.add_argument('--kindex',dest='kindex',help="Enter the fasta file to be used to construct index",metavar="<path to file>",default="Na")
@@ -188,6 +200,7 @@ def main():
 	novel.add_argument('--bowmode',dest="bowalgo",help="Specify the bowtie mode", choices=['b2-very-fast','b2-fast','b2-sensitive','b2-very-sensitive'], default="Na")		
 	novel.add_argument('-g','--gtf',dest="gtf",help="Enter the GTF file to be used for annotations",metavar="<path to file>",default="Na")	
 	novel.add_argument('-u','--multi-read-correct',dest="multi",help="Use 'rescue method' for multi-reads(more accurate)",choices=['True','False'],default="False")
+	novel.add_argument('-m','--mask-file',dest="mask",help="Ignore all alignment within transcripts in this file",metavar="<path to file>",default="Na")
 
 	post = parser.add_argument_group("Post Mapping Metrics Graph")
 	post.add_argument('-p','--post',dest="post",help="Use this option if you want post mapping metrics graph [Default=False]", action="store_const",const="True",default="False")
@@ -258,22 +271,33 @@ def main():
 				logger.error("GTF file not readable")
 				sys.exit()
 
+		if (args.mask == "Na"):
+                        logger.warning("Mask file for ignoring rRNA contamination not specified. This is a recommended option.")
+                else:
+                        if(not os.path.exists(args.mask)):
+                                logger.error("Mask file not readable")
+                                sys.exit()
+
+
 	read1 = args.read1
 	read2 = args.read2
+	
+	#Creating Directory Structure
+	call(['mkdir',args.out])
 
 	#Pre Processing Data
 	if (args.skippre=="False"):
 		log = logging.getLogger('Pre Processing')
         	log.setLevel(logging.DEBUG)
         	log.addHandler(stream)
-
-		call(['mkdir','FastqcMetrics'])
-		Quality_Assessment(args.read1,args.fastqck,log)
-		Quality_Assessment(args.read2,args.fastqck,log)
+		
+		call(['mkdir',args.out+'/FastqcMetrics'])
+		Quality_Assessment(args.read1,args.fastqck,log,args.out)
+		Quality_Assessment(args.read2,args.fastqck,log,args.out)
 
 		TrimmingPE(args.read1,args.read2,args.thread,args.phred,args.trimlead,args.trimtrail,args.trimcrop,args.trimlen,args.trimwindow,args.trimq,args.out,logger)
-		read1 = args.out+"_paired1.fq"
-		read2 = args.out+"_paired2.fq"
+		read1 = args.out+"/"+args.out+"_paired1.fq"
+		read2 = args.out+"/"+args.out+"_paired2.fq"
 	else:
 		logger.info("User opted to skip Pre Processing step")
 
@@ -289,14 +313,14 @@ def main():
 
 	if args.type=="F":
 		log.info("User opted for fast analysis, Prepping for Kallisto...")
-		Kallisto(read1,read2,args.kindex,args.kkmer,args.kboost,args.thread,logger)
+		Kallisto(read1,read2,args.kindex,args.kkmer,args.kboost,args.thread,logger,args.out)
 
 	elif args.type=="S":
 		log.info("User opted for slow analysis, Prepping for bwa and eXpress...")
 		extra = ""
 		if args.strand != "None":
 			extra = "--"+args.strand
-		Bwa(read1,read2,args.bindex,args.algo,extra,logger)
+		Bwa(read1,read2,args.bindex,args.algo,extra,logger,args.out)
 	else:
 		log.info("User opted to perform analysis to detect novel transcripts, Prepping for Tophat, Cufflinks and eXpress...")	
 		libtype = ""
@@ -311,7 +335,7 @@ def main():
 			BowtieIndex(args.bowref,args.out,logger,libtype,bowalgo,read1,read2,args.thread,args.gtf,args.multi)
 		else:
 			print args.bowindex
-			TopHat(read1,read2,args.bowindex,args.out,libtype,bowalgo,args.thread,logger,args.gtf,args.multi)
+			TopHat(read1,read2,args.bowindex,args.out,libtype,bowalgo,args.thread,logger,args.gtf,args.multi,args.mask)
 
 if __name__ == "__main__":
 	main()
